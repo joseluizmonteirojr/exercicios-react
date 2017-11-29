f350fd2f81d5b6ba4884f0c5f05663886ae901c1
zezinho_monteirojr <zezinho_monteirojr@hotmail.com>
2017-11-28 23:39:59 -0200

    Seção 3 - Aula 17 - Configurando o build com webpack

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+    entry: path.join(__dirname, 'src', 'index'),
+    output:{
+        path: path.join(__dirname, 'dist'),
+        filename: './bundle.js'
+    },
+    devServer:{
+        port: 8080,
+        contentBase: './dist'
+    },
+    module:{
+        rules:[{
+            test: /.jsx?$/,
+            use: 'babel-loader',
+            exclude: /node_modules/
+        }]
+    }
+
+}
