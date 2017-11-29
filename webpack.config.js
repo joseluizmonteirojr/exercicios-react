a0431ec800ad1dc5f9fa02bd98c14b323c2ea128
zezinho_monteirojr <zezinho_monteirojr@hotmail.com>
2017-11-28 23:59:55 -0200

    Seção 3 - Aula 19 - Olá React!

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
-    entry: path.join(__dirname, 'src', 'index'),
+    entry: path.join(__dirname, 'src', 'index.jsx'),
     output:{
         path: path.join(__dirname, 'dist'),
         filename: './bundle.js'
